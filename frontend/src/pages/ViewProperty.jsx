bd078d2416e2935df0645942df159cf9ff2e0aa0
muhammad-atif-74 <muhammadatif974.edu.pk@gmail.com>
2024-04-08 23:37:08 +0500

    Property Detail page development

--- frontend/src/pages/ViewProperty.jsx
+++ frontend/src/pages/ViewProperty.jsx
@@ -6,11 +6,59 @@ import ContactUs from '../components/sections/ContactUs'
 import Footer from '../components/sections/Footer'
 import BlogSection from '../components/sections/BlogSection'
 import sellerImage from '../assets/images/sellerImage.png'
-import { FaEnvelope, FaHeart, FaPhone, FaRegHeart } from 'react-icons/fa6'
+import { FaBath, FaBed, FaBuilding, FaEnvelope, FaHeart, FaPhone, FaRegHeart, FaRoadSpikes } from 'react-icons/fa6'
 import MessageOwner from '../components/utils/MessageOwner'
+import { FaRegArrowAltCircleRight, FaSwimmingPool } from 'react-icons/fa'
+
+import { LuBath } from "react-icons/lu";
+import { BsBuildings } from "react-icons/bs";
+import { TbBed } from 'react-icons/tb'
+import { PiFlowerTulip } from "react-icons/pi";
+import { MdOutlinePool } from "react-icons/md";
+import { MdOutlineGarage } from "react-icons/md";
+
+
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
 export default function ViewProperty() {
     const [t] = useTranslation()
     const [favourite, setFavourite] = useState(false)
+    const [mainImage, setMainImage] = useState('');
+
+    const options = {
+        loop: true,
+        center: false,
+        items: 1,
+        margin: 10,
+        autoplay: false,
+        dots: true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        nav: true,
+        responsive: {
+            0: {
+                items: 3
+            },
+            700: {
+                items: 4
+            },
+            1000: {
+                items: 4
+            }
+        }
+    };
+
+    const propertyImages = [
+        "https://media.istockphoto.com/id/1165384568/photo/europe-modern-complex-of-residential-buildings.jpg?s=612x612&w=0&k=20&c=iW4NBiMPKEuvaA7h8wIsPHikhS64eR-5EVPfjQ9GPOA=",
+        "https://img.lovepik.com/photo/48012/2630.jpg_wh860.jpg",
+        "https://assets.architecturaldesigns.com/plan_assets/3199/original/3199D_front_1557350260.jpg"
+
+    ];
+    const handleImageClick = (image) => {
+        setMainImage(image);
+    };
 
     return (
         <>
@@ -21,13 +69,32 @@ export default function ViewProperty() {
                 <div className="custom-container">
                     <div className="split">
                         <div className="product-details side-lg">
+
+                            <div className="side images mb-2">
+                                <div className="main-image">
+                                    <img src={mainImage} alt="" />
+                                </div>
+
+                                <div className="more-images">
+                                    {/* <OwlCarousel id="" className="owl-carousel owl-theme" {...options}> */}
+                                        {propertyImages.map((image, i) => (
+                                            <div key={i} className={`image ${mainImage === image ? 'active' : ''}`} onClick={() => handleImageClick(image)}>
+                                                <img src={image} alt="" />
+                                            </div>
+                                        ))}
+
+
+                                    {/* </OwlCarousel> */}
+                                </div>
+                            </div>
+
                             <div className="side basic-information mb-2">
                                 <div className="d-flex align-items-center justify-content-between mb-2">
                                     <h5 className="color-primary fw-bolder">MAD 23,000</h5>
                                     <div className="custom-badge">Rent</div>
                                 </div>
 
-                                <h3 className="fw-bolder mb-2">Beautiful Modern House with Stunning Views</h3>
+                                <h4 className="fw-bolder mb-2">Beautiful Modern House with Stunning Views</h4>
                                 <p className='paragraph mb-0 '>Appartment, (200 sq ft)</p>
                                 <p className='paragraph color-secondary my-2 '>5232 North Carolina Ave. 21BC</p>
 
@@ -37,16 +104,109 @@ export default function ViewProperty() {
                                         favourite ? (<FaHeart className='icon favourite-icon filled' onClick={() => { setFavourite(!favourite) }} />) : (<FaRegHeart className='icon favourite-icon' onClick={() => { setFavourite(!favourite) }} />)
                                     }
                                 </div>
+
+                                <hr />
+
+                                <div className="info-icons mt-2">
+                                    <div className="d-flex align-items-center gap-2">
+                                        <TbBed className='color-secondary' />
+                                        <small className="color-secondary">2 {t("bedrooms")}</small>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-2">
+                                        <LuBath className='color-secondary' />
+                                        <small className="color-secondary">2 {t("baths")}</small>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-2">
+                                        <BsBuildings className='color-secondary' />
+                                        <small className="color-secondary">2 sq ft</small>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-2">
+                                        <PiFlowerTulip className='color-secondary' />
+                                        <small className="color-secondary">2 {t("garden")}</small>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-2">
+                                        <MdOutlineGarage className='color-secondary' />
+                                        <small className="color-secondary">2 {t("garage")}</small>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-2">
+                                        <MdOutlinePool className='color-secondary' />
+                                        <small className="color-secondary">2 {t("pool")}</small>
+                                    </div>
+                                </div>
                             </div>
 
                             <div className="side description mb-2">
-                                <h3 className="fw-bolder mb-3">{t("description")}</h3>
+                                <h4 className="fw-bolder mb-3">{t("description")}</h4>
                                 <p className="paragraph color-secondary mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora autem ratione nobis ipsa deleniti amet delectus obcaecati qui nihil sed at, mollitia minus recusandae soluta? Nesciunt a fuga veritatis cumque quidem nulla deleniti.</p>
                                 <p className="paragraph color-secondary mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora autem ratione nobis ipsa deleniti amet delectus obcaecati qui nihil sed at, mollitia minus recusandae soluta? Nesciunt a fuga veritatis cumque quidem nulla deleniti.</p>
                                 <p className="paragraph color-secondary mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora autem ratione nobis ipsa deleniti amet delectus obcaecati qui nihil sed at, mollitia minus recusandae soluta? Nesciunt a fuga veritatis cumque quidem nulla deleniti.</p>
 
                             </div>
+
+                            <div className="side more-details">
+                                <h4 className="fw-bolder mb-4">{t("moreDetails")}</h4>
+
+                                <div className="features-grid">
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("category")}</p>
+                                        </div>
+                                        <p className="paragraph">Apartment</p>
+                                    </div>
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("sizeArea")}</p>
+                                        </div>
+                                        <p className="paragraph">200 sq ft</p>
+                                    </div>
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("bedrooms")}</p>
+                                        </div>
+                                        <p className="paragraph">4</p>
+                                    </div>
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("baths")}</p>
+                                        </div>
+                                        <p className="paragraph">2</p>
+                                    </div>
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("garden")}</p>
+                                        </div>
+                                        <p className="paragraph">1</p>
+                                    </div>
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("pool")}</p>
+                                        </div>
+                                        <p className="paragraph">1</p>
+                                    </div>
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("garage")}</p>
+                                        </div>
+                                        <p className="paragraph">2</p>
+                                    </div>
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("type")}</p>
+                                        </div>
+                                        <p className="paragraph">Sale</p>
+                                    </div>
+                                    <div className="feature">
+                                        <div className="label">
+                                            <p className="color-secondary">{t("buildYear")}</p>
+                                        </div>
+                                        <p className="paragraph">2022</p>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
+
+
                         <div className="seller-details side-sm">
                             <div className="side mb-2">
                                 <div className="seller-info mb-3">
@@ -64,6 +224,8 @@ export default function ViewProperty() {
 
                             <MessageOwner />
                         </div>
+
+
                     </div>
                 </div>
             </section>
